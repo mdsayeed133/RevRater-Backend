8f698e13b256759cb327896b41ffe40618561ef9
Mohammed Sayeed Islam <120868662+mdsayeed133@users.noreply.github.com>
2023-02-25 15:29:28 -0800

    fix cross-origin

--- social-media-spring-main/src/main/java/com/revature/controllers/EmployeeSearchController.java
+++ social-media-spring-main/src/main/java/com/revature/controllers/EmployeeSearchController.java
@@ -7,16 +7,14 @@ import com.revature.models.Employee;
 import com.revature.services.EmployeeSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/employee/advance/search")
+@CrossOrigin
 public class EmployeeSearchController {
 
     private EmployeeSearchService employeeSearchService;
